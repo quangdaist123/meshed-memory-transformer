55ec485010346001aa84023226379c383281f839
Lov <quangdaist01@gmail.com>
2021-10-04 15:17:27 +0700

    change batchsize

--- train_fast.py
+++ train_fast.py
@@ -139,7 +139,7 @@ if __name__ == '__main__':
     device = torch.device('cuda')
     parser = argparse.ArgumentParser(description='Meshed-Memory Transformer')
     parser.add_argument('--exp_name', type=str, default="")
-    parser.add_argument('--batch_size', type=int, default=8)
+    parser.add_argument('--batch_size', type=int, default=64)
     parser.add_argument('--workers', type=int, default=8)
     parser.add_argument('--head', type=int, default=8)
     parser.add_argument('--warmup', type=int, default=10000)
