15eadb7f96751e0f09198e2e87e2b911d66f1ab5
Lov <quangdaist01@gmail.com>
2021-10-16 18:19:29 +0700

    faster training time

--- data/field_for_test.py
+++ data/field_for_test.py
@@ -102,6 +102,16 @@ class ImageDetectionsField(RawField):
             else:
                 self.detections_path = tmp_detections_path
 
+
+
+        self.f_train = h5py.File(
+            "/content/drive/MyDrive/ColabNotebooks/UIT-MeshedMemoryTransformer/VieCap4H/viecap4h_train_detections"
+            ".hdf5",
+            'r')
+        self.f_val = h5py.File(
+            "/content/drive/MyDrive/ColabNotebooks/UIT-MeshedMemoryTransformer/VieCap4H/viecap4h_val_detections"
+            ".hdf5",
+            'r')
         super(ImageDetectionsField, self).__init__(preprocessing, postprocessing)
 
     def preprocess(self, x, avoid_precomp=False):
@@ -111,20 +121,14 @@ class ImageDetectionsField(RawField):
         ff.write("\n")
         ff.close()
         try:
-            f_train = h5py.File(
-                "/content/drive/MyDrive/ColabNotebooks/UIT-MeshedMemoryTransformer/VieCap4H/viecap4h_test_detections.hdf5",
-                'r')
-            f_val = h5py.File(
-                "/content/drive/MyDrive/ColabNotebooks/UIT-MeshedMemoryTransformer/VieCap4H/viecap4h_test_detections.hdf5",
-                'r')
-            if image_id in list(f_train.keys()):
-                precomp_data = f_train['%s' % image_id]["features"][()]
+            if image_id in list(self.f_train.keys()):
+                precomp_data = self.f_train['%s' % image_id]["features"][()]
                 if self.sort_by_prob:
-                    precomp_data = precomp_data[np.argsort(np.max(f_train['%s_cls_prob' % image_id][()], -1))[::-1]]
-            if image_id in list(f_val.keys()):
-                precomp_data = f_val['%s' % image_id]["features"][()]
+                    precomp_data = precomp_data[np.argsort(np.max(self.f_train['%s_cls_prob' % image_id][()], -1))[::-1]]
+            if image_id in list(self.f_val.keys()):
+                precomp_data = self.f_val['%s' % image_id]["features"][()]
                 if self.sort_by_prob:
-                    precomp_data = precomp_data[np.argsort(np.max(f_val['%s_cls_prob' % image_id][()], -1))[::-1]]
+                    precomp_data = precomp_data[np.argsort(np.max(self.f_val['%s_cls_prob' % image_id][()], -1))[::-1]]
         except KeyError:
             warnings.warn('Could not find detections for %s' % image_id)
             precomp_data = np.random.rand(10,2048)
